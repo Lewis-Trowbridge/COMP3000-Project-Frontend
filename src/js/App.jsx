51b04f5952fabea5a02734984887bcf1708d1512
Lewis Trowbridge <lewbob9927@gmail.com>
2022-11-16 13:02:00 +0000

    CFYP-38 - Restricted backend call to current position

--- src/js/App.jsx
+++ src/js/App.jsx
@@ -1,17 +1,6 @@
-import useBackend from './utils/useBackend'
 import Map from './components/Map'
 
 function App() {
-  // eslint-disable-next-line no-unused-vars
-  const { data } = useBackend({
-    bbox: {
-      bottomLeftX: 50.35535618196109,
-      bottomLeftY: -4.230367299744433,
-      topRightX: 50.480183908774855,
-      topRightY: -4.013752232325168,
-    },
-  })
-
   return (
     <Map />
   )

63f3086f145022f3068ee872445cf5b50f86ce54
Lewis Trowbridge <lewbob9927@gmail.com>
2022-11-16 22:59:24 +0000

    CFYP-37 - Fixed marker PNG issue

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 import { createRoot } from 'react-dom/client'
+import L from 'leaflet'
+import icon from 'leaflet/dist/images/marker-icon.png'
+import iconShadow from 'leaflet/dist/images/marker-shadow.png'
+import iconRetina from 'leaflet/dist/images/marker-icon-2x.png'
 import App from './js/App'
+
+// eslint-disable-next-line no-underscore-dangle
+delete L.Icon.Default.prototype._getIconUrl
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: iconRetina,
+  iconUrl: icon,
+  shadowUrl: iconShadow,
+})
 
 const container = document.getElementById('root')
 const root = createRoot(container)

b0944477756012eff0f23653a961b7345a285a4f
Lewis Trowbridge <lewbob9927@gmail.com>
2022-11-17 23:49:05 +0000

    CFYP-37 - Added tests for rendering markers

--- src/js/components/__tests__/Stations.test.jsx
+++ src/js/components/__tests__/Stations.test.jsx
@@ -1,11 +1,8 @@
-import { render } from '@testing-library/react'
+import { act, render } from '@testing-library/react'
+import { useMapEvents } from 'react-leaflet'
 import Stations from '../Stations'
 import { BACKEND_RESPONSES } from '../../testConstants'
 import useBackend from '../../utils/useBackend'
-
-const mockMap = {
-  getBounds: jest.fn().mockReturnValue({}),
-}
 
 jest.mock('react-leaflet', () => ({
   // eslint-disable-next-line react/prop-types
@@ -15,7 +12,18 @@ jest.mock('react-leaflet', () => ({
       {`${position.lat}, ${position.lng}`}
     </p>
   ),
-  useMapEvents: jest.fn().mockReturnValue(mockMap),
+  useMapEvents: jest.fn().mockReturnValue({
+    getBounds: jest.fn().mockReturnValue({
+      getNorthEast: () => ({
+        lat: 0,
+        lng: 0,
+      }),
+      getSouthWest: () => ({
+        lat: 0,
+        lng: 0,
+      }),
+    }),
+  }),
 }))
 
 jest.mock('../../utils/useBackend', () => ({
@@ -30,5 +38,41 @@ describe('<Stations/>', () => {
     const { findByText } = render(<Stations />)
     const expectedText = `${BACKEND_RESPONSES.VALID.station.coordinates.lat}, ${BACKEND_RESPONSES.VALID.station.coordinates.lng}`
     expect(await findByText(expectedText)).toBeInTheDocument()
+  })
+
+  it('does not render markers when an empty list is returned', async () => {
+    useBackend.mockReturnValueOnce({ data: [] })
+
+    const { queryByText } = render(<Stations />)
+    const expectedText = `${BACKEND_RESPONSES.VALID.station.coordinates.lat}, ${BACKEND_RESPONSES.VALID.station.coordinates.lng}`
+    expect(await queryByText(expectedText)).not.toBeInTheDocument()
+  })
+
+  it('rerenders when the load event fires', async () => {
+    useBackend.mockReturnValueOnce({ data: [] })
+    useBackend.mockReturnValueOnce({ data: [BACKEND_RESPONSES.VALID] })
+
+    const { queryByText } = render(<Stations />)
+
+    const expectedText = `${BACKEND_RESPONSES.VALID.station.coordinates.lat}, ${BACKEND_RESPONSES.VALID.station.coordinates.lng}`
+    expect(await queryByText(expectedText)).not.toBeInTheDocument()
+    await act(() => {
+      useMapEvents.mock.lastCall[0].load()
+    })
+    expect(await queryByText(expectedText)).toBeInTheDocument()
+  })
+
+  it('rerenders when the moveend event fires', async () => {
+    useBackend.mockReturnValueOnce({ data: [] })
+    useBackend.mockReturnValueOnce({ data: [BACKEND_RESPONSES.VALID] })
+
+    const { queryByText } = render(<Stations />)
+
+    const expectedText = `${BACKEND_RESPONSES.VALID.station.coordinates.lat}, ${BACKEND_RESPONSES.VALID.station.coordinates.lng}`
+    expect(await queryByText(expectedText)).not.toBeInTheDocument()
+    await act(() => {
+      useMapEvents.mock.lastCall[0].moveend()
+    })
+    expect(await queryByText(expectedText)).toBeInTheDocument()
   })
 })

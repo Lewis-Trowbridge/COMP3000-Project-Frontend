c067ac3abd2558db3e8141d2648ab17ebfc6b847
Lewis Trowbridge <lewbob9927@gmail.com>
2022-11-20 17:31:04 +0000

    CFYP-50 - Used jQuery selector in test

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -3,6 +3,7 @@ describe('empty spec', () => {
     cy.visit('http://localhost:1234')
   })
   it('loads a map on load', () => {
-    expect('.leaflet-tile-container').to.have.descendants('img.leaflet-tile')
+    // eslint-disable-next-line no-undef
+    expect($('.leaflet-tile-container')).to.have.descendants('img.leaflet-tile')
   })
 })

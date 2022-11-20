6338960a080fc36feda4ec906cb9f180762103df
Lewis Trowbridge <lewbob9927@gmail.com>
2022-11-20 17:26:13 +0000

    CFYP-50 - Added preliminary test for images appearing on load

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,8 @@
 describe('empty spec', () => {
-  it('passes', () => {
+  beforeEach(() => {
     cy.visit('http://localhost:1234')
+  })
+  it('loads a map on load', () => {
+    expect('.leaflet-tile-container').to.have.descendants('img.leaflet-tile')
   })
 })

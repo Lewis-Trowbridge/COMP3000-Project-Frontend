bde929041a43f2618534165e071da9a9241c66dd
Lewis Trowbridge <lewis.trowbridge@students.plymouth.ac.uk>
2023-02-01 12:52:56 +0000

    CFYP-64 - Added constants

--- src/js/constants.js
+++ src/js/constants.js
@@ -9,8 +9,15 @@ const LEAFLET_POSITION_CLASSES = {
   topright: 'leaflet-top leaflet-right',
 }
 
+const TIME_VALUES = {
+  JAN_1_1990_UNIX_TIMESTAMP: 631152000000,
+  ONE_MONTH_IN_MS: 26298000000,
+  THREE_YEARS_IN_MS: 94670778000,
+}
+
 export {
   // eslint-disable-next-line import/prefer-default-export
   URLS,
   LEAFLET_POSITION_CLASSES,
+  TIME_VALUES,
 }

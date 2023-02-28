d6b6bb7f82b2d6820e9c70ca2a2a2d0bd53f1419
Lewis Trowbridge <lewis.trowbridge@students.plymouth.ac.uk>
2023-02-28 10:12:19 +0000

    CFYP-81 - Used d3-delaunay to display WIP polygons

--- src/js/components/Stations.jsx
+++ src/js/components/Stations.jsx
@@ -1,19 +1,43 @@
-import { Marker } from 'react-leaflet'
-import { useContext } from 'react'
+import { Polygon } from 'react-leaflet'
+import {
+  useContext, useMemo,
+} from 'react'
+import { Delaunay } from 'd3-delaunay'
 import ReadingContext from '../utils/ReadingContext'
 
 const Stations = () => {
-  const { data, setSelected } = useContext(ReadingContext)
+  // eslint-disable-next-line no-unused-vars
+  const { bounds, data, setSelected } = useContext(ReadingContext)
+
+  const polygonCoords = useMemo(() => {
+    if (bounds && data) {
+      const bbox = [
+        bounds.bottomLeftY,
+        bounds.bottomLeftX,
+        bounds.topRightY,
+        bounds.topRightX,
+      ]
+      const sites = data.map((reading) => ([
+        reading.station.coordinates.lng, reading.station.coordinates.lat]))
+      const voronoi = Delaunay.from(sites).voronoi(bbox)
+      const polygons = []
+      // eslint-disable-next-line no-restricted-syntax
+      for (const point of voronoi.cellPolygons()) {
+        polygons.push(point.map((lngLatArr) => ([lngLatArr[1], lngLatArr[0]])))
+      }
+      return polygons
+    }
+    return []
+  }, [bounds, data])
+
   return (
     <div>
-      {data.map((reading) => (
-        <Marker
-          key={reading.station.name}
-          position={reading.station.coordinates}
+      {polygonCoords.map((coords) => (
+        <Polygon
+          positions={coords}
           eventHandlers={{
-            click: () => {
-              setSelected(reading)
-            },
+            // eslint-disable-next-line no-console
+            click: () => console.log(coords),
           }}
         />
       ))}
